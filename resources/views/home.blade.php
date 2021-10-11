523aa7427fcb0f90912f28890cc1017b74a49ec6
mirkone1994 <mirkochiappori@gmail.com>
2021-10-11 15:47:49 +0200

    reate home controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,7 @@
 @section('title', 'Home')
 @section('section-id', 'home')
 @section('content')
-
+    <div class="card-title">
+        <h1 class="text-center">Comics</h1>
+    </div>
 @endsection
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,8 +1,20 @@
-<nav class="navbar navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-        Bootstrap
-      </a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="{{ asset('images/dc-logo.png')}}" alt="" width="30" class="d-inline-block align-text-top img-fluid me-2">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" aria-current="page" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ request()->routeIs('comics.index') ? 'active' : '' }}" href="#">Comics</a>
+        </li>
+      </ul>
     </div>
-  </nav>
+  </div>
+</nav>
